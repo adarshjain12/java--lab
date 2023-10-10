4f49c6c7e9f3a4e2afaf893a61192d76274aabb0
adarshjain12 <143694733+adarshjain12@users.noreply.github.com>
2023-10-10 12:52:21 +0530

    Add files via upload

--- Main (6).java
+++ Main (6).java
@@ -1,17 +1,34 @@
-import java.util.Scanner;
-public class Main{
- public static void main(String args[]){
- System.out.println("Enter any number: ");
- Scanner ob1=new Scanner(System.in);
- int a=ob1.nextInt();
- if(a>0){
- System.out.println("The number is positive.");
- }else if(a<0){
- System.out.println("The number is negative.");
- }
- else{
- System.out.println("Zero");
- }
- }
+class Parent {
+    void display() {
+        System.out.println("This is the parent class");
+    }
+
+    void show(int num) {
+        System.out.println("The number in the parent class is: " + num);
+    }
 }
 
+class Child extends Parent {
+    @Override
+    void display() {
+        System.out.println("This is the child class");
+    }
+
+    void show(double num) {
+        System.out.println("The number in the child class is: " + num);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Parent parentObj = new Parent();
+        Parent childObj = new Child();
+
+     
+        parentObj.show(5);     
+        childObj.show(7);  
+
+          parentObj.display();   
+        childObj.display();   
+}
+}
