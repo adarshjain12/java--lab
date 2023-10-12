634b71327524e6d54ff33f543a94d941c2d7373d
adarshjain12 <143694733+adarshjain12@users.noreply.github.com>
2023-10-12 12:33:50 +0530

    Add files via upload

--- A.java
+++ A.java
@@ -1,24 +1,28 @@
-import java.util.Scanner;
-public class A{
- public static void main(String args[]){
- System.out.println("Enter any two numbers: ");
- Scanner sc=new Scanner(System.in);
- int a=sc.nextInt();
- int b=sc.nextInt();
- int gcd=1;
- for(int i=1;i<=a&&i<=b;i++){
- if(a%i==0&&b%i==0){
- gcd=i;
- }
- }
- System.out.println("The GCD of the numbers is: "+ gcd);
- }
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+interface Solution {
+    public void Hello();
+    public void Welcome();
 }
 
+public class A implements Solution {
+    public void Hello() {
+        System.out.println("Hello world");
+    }
 
+    public void Welcome() {
+        System.out.println("Welcome to Edureka");
+    }
 
-
- 
-
-
-
+    public static void main(String[] args) {
+        A a = new A();
+        a.Hello();
+        a.Welcome();
+    }
+}
